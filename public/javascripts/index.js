49792e3cab72cbfd3e54e146598dd73a3e6700f7
Andrew <andrewespn@hotmail.com>
2019-02-13 20:57:19 -0800

    Updated form html and javscript searching

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,7 +8,27 @@ function search() {
 		xhr.send();
 		xhr.onload = function (response) {
 			console.log(response);
-			document.getElementById('information').innerHTML = this.responseText;
+			// document.getElementById('information').innerHTML = this.responseText;
+			let data = JSON.parse(this.responseText);
+			let list = document.createElement('ul');
+			for (let i = 0; i < data.length; i++){
+				let road = document.createElement('li');
+				// let roadbutton = document.createElement('button');
+				road.classList.add('road-list');
+				// roadbutton.classList.add('button-list');
+				// road.innerHTML = data[i]['FULL_NAME'] + ' ' + data[i]['LEFTZIP'];
+				// list.append(roadbutton);
+				let str = `<div class="card" style="width: 70vw;">
+				<div class="card-body">
+				  <h5 class="card-title">${data[i]['FULL_NAME']} ${data[i]['LEFTZIP']}</h5>
+				  <a href= "/dataReviews.html" class="btn btn-primary">Rate Road</a>
+				</div>
+			  </div>`;
+			  road.innerHTML = str;
+			  list.appendChild(road);
+			}
+			document.getElementById('information').appendChild(list);
+
 			return response;
 		}
 	} else {
